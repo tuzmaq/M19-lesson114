7aaea4433baceb4112de50470a46d9c11b62ca93
tuzmaq <tuzmaq@gmail.com>
2024-09-15 12:52:02 +0530

    L113

---
+++ current.py
@@ -0,0 +1,9 @@
+import datetime as dt  
+    
+# using now() to get current time  
+current_time = dt.datetime.now()  
+    
+# Printing value of now.  
+print("Time now at Malaysia is : ", end = "")
+
+print(current_time)
